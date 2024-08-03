6ce2aa8aa83f9936e640537cc5dab0d91c1c3ab7
disha-navsoft <dishaarora1996@gmail.com>
2024-08-03 16:34:06 +0530

    added

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,7 +3,7 @@ set -e
 
 # Run collectstatic command
 echo "Running collectstatic command..."
-python manage.py collectstatic --noinput
+pyth÷on manage.py collectstatic --noinput
 
 # Restart Gunicorn and Nginx services
 echo "Restarting Gunicorn and Nginx services..."

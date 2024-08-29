e5da90d7d7a2958dd10146765f01a18f4093fa89
dishaarora1996 <89445480+dishaarora1996@users.noreply.github.com>
2024-08-29 16:40:14 +0530

    Update nginx.sh

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -13,10 +13,12 @@ sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
 # Copy Nginx configuration file
-sudo cp "/home/ubuntu/project/$PROJECT_MAIN_DIR_NAME/nginx/nginx.conf" "/etc/nginx/sites-available/$FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS"
+# sudo cp "/home/ubuntu/project/$PROJECT_MAIN_DIR_NAME/nginx/nginx.conf" "/etc/nginx/sites-available/$FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS"
+sudo cp "/home/ubuntu/project/TazosMon-Backend/nginx/nginx.conf" "/etc/nginx/sites-available/bba"
 
 # Create symbolic link to enable Nginx site
-sudo ln -s "/etc/nginx/sites-available/$FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS" "/etc/nginx/sites-enabled/"
+# sudo ln -s "/etc/nginx/sites-available/$FOLDER_NAME_WHERE_SETTINGS_FILE_EXISTS" "/etc/nginx/sites-enabled/"
+sudo ln -sf "/etc/nginx/sites-available/bba" "/etc/nginx/sites-enabled/"
 
 # Add www-data user to ubuntu group
 sudo gpasswd -a www-data ubuntu

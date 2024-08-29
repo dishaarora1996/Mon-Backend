4761aeee33994c1ea3fdfb6fc46475d3c13528cc
dishaarora1996 <89445480+dishaarora1996@users.noreply.github.com>
2024-08-29 14:50:54 +0530

    Update gunicorn.sh

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 
 # Replace {YOUR_PROJECT_MAIN_DIR_NAME} with your actual project directory name
-PROJECT_MAIN_DIR_NAME="Mon-Backend"
+PROJECT_MAIN_DIR_NAME="TazosMon-Backend"
 
 # Copy gunicorn socket and service files
-sudo cp "/home/ubuntu/project/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.socket" "/etc/systemd/system/gunicorn.socket"
-sudo cp "/home/ubuntu/project/$PROJECT_MAIN_DIR_NAME/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
+sudo cp "/home/ubuntu/project/TazosMon-Backend/gunicorn/gunicorn.socket" "/etc/systemd/system/gunicorn.socket"
+sudo cp "/home/ubuntu/project/TazosMon-Backend/gunicorn/gunicorn.service" "/etc/systemd/system/gunicorn.service"
 
 # Start and enable Gunicorn service
 sudo systemctl start gunicorn.service
